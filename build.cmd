8df4745a30166cce5e67d5fdc0874dde92e0d280
torph <73564623+Torphedo@users.noreply.github.com>
2023-09-07 14:08:33 -0400

    Add Zig build script This should work on both Windows and Linux. Sorry there's no comments in it, there's no cross-platform way to do that in a script as far as I know...

---
+++ build.cmd
@@ -0,0 +1,3 @@
+zig cc -g0 -o main.s -target powerpc-freestanding -S -O3 -mbig-endian -m32 -exceptions -fno-ident -s -mllvm -ppc-asm-full-reg-names main.c
+
+cemu_patch_tool main.s BOTW_CustomCombos 0x02d5b828 patch_main.asm
